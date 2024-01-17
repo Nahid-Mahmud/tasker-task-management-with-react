e40e3100b04089987eb562c583b2f0d1cfa33f2a
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-01-18 01:10:51 +0600

    Tag data dynamically showed from state

--- src/Components/Task/TaskBoard.jsx
+++ src/Components/Task/TaskBoard.jsx
@@ -1,8 +1,30 @@
+import { useState } from "react";
 import Search from "./Search";
 import TaskAction from "./TaskAction";
 import TaskLilts from "./TaskLilts";
 
 const TaskBoard = () => {
+  // Default Task
+  // const defaultTask = {
+  //   id: crypto.randomUUID(),
+  //   title: "Learn React Native",
+  //   description: "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+  //   tags: ["web", "react", "js"],
+  //   priority: "High",
+  //   isFavorite: true,
+  // };
+
+  const [tasks, setTasks] = useState([
+    {
+      id: crypto.randomUUID(),
+      title: "Learn React Native",
+      description: "I want to Learn React such thanI can treat it like my slave and make it do whatever I want to do.",
+      tags: ["web", "react", "js"],
+      priority: "High",
+      isFavorite: true,
+    },
+  ]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
@@ -13,7 +35,7 @@ const TaskBoard = () => {
         {/* Search Box Ends */}
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskAction />
-          <TaskLilts />
+          <TaskLilts tasks={tasks} />
         </div>
       </div>
     </section>
